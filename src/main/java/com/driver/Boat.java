4f72cb26a1f31976da1e1ed245f7c1ef8b871181
Kowshik1212 <mkowshikreddy@gmail.com>
2024-02-25 18:25:52 +0530

    first commit

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,20 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+    Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+}
